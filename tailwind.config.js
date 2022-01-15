90cae2e7f3f4815a958509e9f0badab23e9edee7
Keigo <morio211106@gmail.com>
2022-01-16 08:34:52 +0900

    fix: font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -170,6 +170,17 @@ module.exports = {
     fontSize: {
       'xxs': '10px',
       'xs': '12px',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem'
     },
     screens: {
       'tiny': "400px",
