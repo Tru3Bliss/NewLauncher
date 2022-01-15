d82269db0f7d0eac3ee84bceb17afdb4e25bdf86
Keigo <morio211106@gmail.com>
2022-01-15 23:46:55 +0900

    1-2-1 Homepage
    make it possible to build

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import * as am5 from "@amcharts/amcharts5";
+import * as am5xy from "@amcharts/amcharts5/xy";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 import { H1, H2, Title } from '../../components/label'
 import Layout from '../../layout/layout'
 import { filterOptions } from './options'
@@ -167,6 +170,82 @@ const HomePage = () => {
     axios.post(BASE_API_URL + "listings").then((response) => setProjects(response.data)).catch((response) => {
       console.log(response.data)
     })
+
+    let root = am5.Root.new("chartdiv");
+
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    let chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        panY: false,
+        layout: root.verticalLayout
+      })
+    );
+
+    // Define data
+    let data = [{
+      category: "Research",
+      value1: 1000,
+      value2: 588
+    }, {
+      category: "Marketing",
+      value1: 1200,
+      value2: 1800
+    }, {
+      category: "Sales",
+      value1: 850,
+      value2: 1230
+    }];
+
+    // Create Y-axis
+    let yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(root, {
+        renderer: am5xy.AxisRendererY.new(root, {})
+      })
+    );
+
+    // Create X-Axis
+    let xAxis = chart.xAxes.push(
+      am5xy.CategoryAxis.new(root, {
+        renderer: am5xy.AxisRendererX.new(root, {}),
+        categoryField: "category"
+      })
+    );
+    xAxis.data.setAll(data);
+
+    // Create series
+    let series1 = chart.series.push(
+      am5xy.ColumnSeries.new(root, {
+        name: "Series",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value1",
+        categoryXField: "category"
+      })
+    );
+    series1.data.setAll(data);
+
+    // let series2 = chart.series.push(
+    //   am5xy.ColumnSeries.new(root, {
+    //     name: "Series",
+    //     xAxis: xAxis,
+    //     yAxis: yAxis,
+    //     valueYField: "value2",
+    //     categoryXField: "category"
+    //   })
+    // );
+    // series2.data.setAll(data);
+
+    // Add legend
+    let legend = chart.children.push(am5.Legend.new(root, {}));
+    legend.data.setAll(chart.series.values);
+
+    // Add cursor
+    chart.set("cursor", am5xy.XYCursor.new(root, {}));
+
+    // this.root = root;
   }, [])
 
   return (
@@ -276,6 +355,7 @@ const HomePage = () => {
           <H2>Developer Sales Volume</H2>
           <div className='text-app-black-80 mt-4'>Based on sales from Nov 19 to Nov 21</div>
           <div className='mt-10 overflow-x-auto'>
+            <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
           </div>
         </div>
 
@@ -315,3 +395,4 @@ const HomePage = () => {
 }
 
 export default HomePage;
+
